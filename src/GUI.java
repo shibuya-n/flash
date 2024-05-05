28759db7350752d11ecdf3723b20f8089b56f84f
shibuya-n <109704872+shibuya-n@users.noreply.github.com>
2024-05-04 17:13:43 -0700

    update

--- src/GUI.java
+++ src/GUI.java
@@ -1,6 +1,46 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
 
+
+    // make middle portion of GUI
+    public JPanel makeMenu(JPanel window){
+        // test array
+        String[] test = {"test1", "test2"};
+
+        JPanel subPanel = new JPanel();
+        subPanel.setLayout(new GridLayout(3, 1));
+
+        // Label
+        JLabel unitDescription = new JLabel("select unit");
+
+        // Dropdown
+        JComboBox unitSelect = new JComboBox(test);
+
+        // Button
+        JButton goButton = new JButton("go");
+
+        goButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+
+            }
+        });
+
+
+
+        subPanel.add(unitDescription);
+        subPanel.add(unitSelect);
+        subPanel.add(goButton);
+
+        window.add(subPanel);
+
+
+
+        return window;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+
 public class Main {
     public static void main(String[] args) {
+        // set up JFrame border layout
+        JFrame.setDefaultLookAndFeelDecorated(false);
+        JFrame window = new JFrame("FLASH REVIEW");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+
+        JPanel menu = new JPanel();
+        menu.setLayout(new FlowLayout());
+
+        GUI makeGUI = new GUI();
+
+        makeGUI.makeMenu(menu);
+
+        mainPanel.add(menu, BorderLayout.CENTER);
+        window.add(mainPanel);
+        window.pack();
+        window.setBounds(500,500,750,750);
+        window.setVisible(true);
+
+
 
     }
 }
