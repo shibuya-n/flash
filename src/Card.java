8b617746e098646907ae660b57d8ec986765d0d4
shibuya-n <a.duong2006@gmail.com>
2024-09-13 14:01:02 -0700

    work on quiz panel

---
+++ src/Card.java
@@ -0,0 +1,19 @@
+import java.io.File;
+
+public class Card {
+
+    File cardFile;
+    String frontDescription;
+    String backDescription;
+
+    public Card (File input, String f, String b){
+
+        cardFile = input;
+        frontDescription = f;
+        backDescription = b;
+
+    }
+
+
+
+}
